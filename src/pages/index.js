be4291c7672a418f6f367798d1534ad8e1f985ab
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-06-20 20:37:43 +0200

    Init gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const HomePage = () => <h1>Hello world!</h1>
+
+export default HomePage
