24548cf8227f68f7ab9e3f40938c9bff4b573806
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-06-23 16:45:21 +0200

    add source plugin - pulling data from wordpress
    add and cofigure plugin to pull data into Gatsby from Wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,13 @@ module.exports = {
         id: 'portal',
       },
     },
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'WPGraphQL',
+        fieldName: 'wpgraphql',
+        url: process.env.WP_GRAPHQL_URL,
+      },
+    },
   ],
 }
