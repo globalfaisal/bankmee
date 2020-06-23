088809e2352252dd55da67e78bb2076efa635e75
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-06-23 21:42:52 +0200

    improve main navigations
    move mobile navigation logic to main-nav component
    improve mobile nav styles and add animation
    add hamburger menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,41 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
-import { useMediaQuery } from 'react-responsive'
 
 import MainNav from '../main-nav/main-nav'
+
 import logoImgUrl from '../../assets/images/logo.svg'
 import styles from './header.module.scss'
 
-import Overlay from '../overlay/overlay'
+const Header = ({ mainMenuItems }) => (
+  <header className={styles.header}>
+    <Link to="/" className={styles.logo}>
+      <img src={logoImgUrl} alt="logo" className={styles.logoImage} />
+      <div className={styles.logoText}>bankmee</div>
+    </Link>
 
-const Header = ({ mainMenuItems }) => {
-  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false)
-  const isMobileOrTablet = useMediaQuery({ query: '(max-width: 768px)' })
-
-  return (
-    <header className={styles.header}>
-      <Link to="/" className={styles.logo}>
-        <img src={logoImgUrl} alt="logo" className={styles.logoImage} />
-        <div className={styles.logoText}>bankmee</div>
-      </Link>
-
-      {isMobileOrTablet && (
-        <div
-          role="presentation"
-          onClick={() => setIsMobileNavOpen(prevState => !prevState)}
-          className={styles.navMenuToggler}
-        >
-          Menu
-        </div>
-      )}
-
-      <MainNav
-        showMobileNav={isMobileOrTablet && isMobileNavOpen}
-        mainMenuItems={mainMenuItems}
-      />
-      {isMobileOrTablet && isMobileNavOpen && <Overlay />}
-    </header>
-  )
-}
+    <MainNav mainMenuItems={mainMenuItems} />
+  </header>
+)
 
 export default Header
--- src/components/main-nav/main-nav.js
+++ src/components/main-nav/main-nav.js
@@ -1,19 +1,38 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import { useMediaQuery } from 'react-responsive'
+
+import hamburgerIconUrl from '../../assets/images/icon-hamburger.svg'
+import closeIconUrl from '../../assets/images/icon-close.svg'
 
 import styles from './main-nav.module.scss'
 
-const MainNav = ({ mainMenuItems, showMobileNav = false }) => {
+const MainNav = ({ mainMenuItems }) => {
+  const [toggle, setToggle] = useState(false)
+  const isMobileOrTablet = useMediaQuery({ query: '(max-width: 768px)' })
+
   if (!mainMenuItems) return null
 
   return (
-    <nav
-      className={`${styles.mainNav} ${
-        showMobileNav ? styles.mobileMenuShown : styles.mobileMenuHidden
-      }`}
-    >
-      <ul className={styles.navList}>
+    <nav className={styles.mainNav}>
+      {isMobileOrTablet && (
+        <>
+          <div
+            role="presentation"
+            onClick={() => setToggle(prevState => !prevState)}
+            className={styles.navMenuToggler}
+          >
+            <img src={toggle ? closeIconUrl : hamburgerIconUrl} alt="Menu" />
+          </div>
+        </>
+      )}
+
+      <ul
+        className={`${styles.navMenus} ${
+          isMobileOrTablet && !toggle ? styles.navMenusHidden : ''
+        }`}
+      >
         {mainMenuItems
           .filter(item => item.url !== '/')
           .map(item => (
@@ -35,7 +54,6 @@ const MainNav = ({ mainMenuItems, showMobileNav = false }) => {
 }
 MainNav.propTypes = {
   mainMenuItems: PropTypes.array.isRequired,
-  showMobileNav: PropTypes.bool,
 }
 
 export default MainNav
